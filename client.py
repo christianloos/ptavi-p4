e6ff2ab26de54f4d7a7bac04d6cf264768509e3f
Christian <christianloos43@gmail.com>
2016-10-17 19:22:42 +0200

    Apartado 6

--- client.py
+++ client.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
-
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
+
 SERVER = str(sys.argv[1])
 PORT = int(sys.argv[2])
-LINE = str(' '.join(sys.argv[3:]))
+line = str(' '.join(sys.argv[3:]))
 
+chops = line.split(' ')
+if sys.argv[3] == 'register':
+        line = str(chops[0].upper() + ' sip: ' + chops[1])
 
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT)) #conecta al servidor 
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n') #b convierte a bytes, r y n son el salto de linea y la sangria
-    data = my_socket.recv(1024) #1024 -- valor del buffer en bytes
+    my_socket.connect((SERVER, PORT))
+    print("Enviando:", line)
+    my_socket.send(bytes(line, 'utf-8') + b' SIP/2.0\r\n\r\n')
+    data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
-# al terminar el with la conexion se cierra automaticamente
 
 print("Socket terminado.")
