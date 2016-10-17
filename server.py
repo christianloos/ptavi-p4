985c34bde396ebddc8e9030691dd86642d44fc0a
Christian <christianloos43@gmail.com>
2016-10-17 19:22:59 +0200

    Apartado 6

--- server.py
+++ server.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
-
 import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler): #clase para manejar peticiones
-    """
-    Echo server class
-    """
-#hereda de DatagramRequestHandler del modulo socketserver
-#no tiene init ya que lo hereda de la clase padre
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):        
-        self.wfile.write(b"Hemos recibido tu peticion") 
-        #wfile y rfile son atributos que abstraen del socket como si fuera un fichero
-        for line in self.rfile: 
-        #rfile va iterando sobre las lineas como si fuera un fichero de lectura
-        #podemos ir escribiendo en el con wfile.write()
-            print("El cliente nos manda: ", line.decode('utf-8')) #envia secuencias de bytes
-            print('IP: ', self.client_address[0])
-            print('Puerto: ', self.client_address[1])
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n") 
+
+        client_data = {}
+        line = self.rfile.read()
+        data = line.decode('utf-8')
+        chops = data.split(' ')
+        if chops[0] == 'REGISTER':
+            client_data[chops[1]] = chops[2]
+            client_data['ip'] = self.client_address[0]
+        
+        print("El cliente nos manda: ", data)
+        print('IP: ', self.client_address[0])
+        print('Puerto: ', self.client_address[1])
+        print('Los datos del cliente son:', client_data)
 
 if __name__ == "__main__":
 
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler) #se instancia la clase EchoHandler indicando IP y puerto donde el servidor escucha
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print("Finalizado servidor") #finaliza escucha con Ctrl+C
-        
+        print()
+        print("Finalizado servidor")   
