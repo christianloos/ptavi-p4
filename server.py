100788571d3a29c8bb2d2d6437fb35989f2ad5c5
Cristian  Loos Beneit <cloos@alpha17.aulas.gsyc.urjc.es>
2016-10-18 15:35:06 +0200

    Apartado 7

--- server.py
+++ server.py
@@ -6,24 +6,23 @@ import sys
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
+    client_dicc = {}
+    
     def handle(self):        
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n") 
 
-        client_data = {}
         line = self.rfile.read()
         data = line.decode('utf-8')
         chops = data.split(' ')
         if chops[0] == 'REGISTER':
-            client_data[chops[1]] = chops[2]
-            client_data['ip'] = self.client_address[0]
-            
-            if chops[4] == 'Expires':
-                del client_data['sip', 'ip']
-        
+            self.client_dicc[chops[1][4:]] = self.client_address[0]
+            if chops[3][2:-1] == 'Expires':
+                if chops[4][:-4] == '0':
+                    del self.client_dicc[chops[1][4:]]
         print("El cliente nos manda:" + '\r\n' + data)
         print('IP: ', self.client_address[0])
         print('Puerto: ', self.client_address[1])
-        print('Los datos del cliente son:', client_data)
+        print('Los datos del cliente son:', self.client_dicc)
         print()
 
 if __name__ == "__main__":
